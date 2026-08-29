bac764d1493ca637099f351f9b54e29d7e2a161c
Mizumi67 <pan3ditz31@gmail.com>
2026-08-29 16:10:53 +0700

    Stroke Order Update

---
+++ js/prettify-url.js
@@ -0,0 +1,20 @@
+(function() {
+    const prettySections = ['home', 'hiragana', 'katakana', 'kotoba', 'games'];
+
+    function getRootBase() {
+        return window.location.pathname.split('/').slice(0, 2).join('/') + '/';
+    }
+
+    function prettifyUrl() {
+        const id = window.location.hash.replace('#', '');
+        if (!prettySections.includes(id)) return;
+        const rootBase = getRootBase();
+        const newPath = id === 'home' ? rootBase : rootBase + id + '/';
+        history.replaceState(null, '', newPath);
+    }
+
+    if (window.location.hash) {
+        prettifyUrl();
+    }
+    window.addEventListener('hashchange', prettifyUrl);
+})();
